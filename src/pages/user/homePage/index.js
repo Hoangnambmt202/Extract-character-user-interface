f345533c25fb2212af771bba37418e79df3a6a21
HoangNam <nam23062002@gmail.com>
2024-08-23 01:23:06 +0700

    css header and main homepage

--- src/pages/user/homePage/index.js
+++ src/pages/user/homePage/index.js
@@ -1,6 +1,22 @@
 const HomePage = () => {
   return (
-    <h2>HomePage</h2>
+    <main className="w-full">
+        <section>
+          <div className="container grid  mx-auto px-4  pb-10" >
+            <div className="max-w-3xl mx-auto pt-32 pb-10" >
+              <h1 className="text-6xl font-bold text-center title  text-white mb-4" >Extract and store characters easily</h1>
+              <h3 className="text-xl sub-title text-center text-white mb-8" >Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</h3>
+              <div className="flex justify-center" > 
+                <a href={{}} className="inline-block mx-2 px-6 py-3 rounded-lg bg-indigo-600 text-white" >Start free trial</a>
+                <a href={{}} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold mx-2 py-3 px-6 rounded-lg inline-flex items-center" >Learn more</a>
+              </div>
+            </div>
+            <div>
+              <video src={{}}></video>
+            </div>
+          </div>
+        </section>
+    </main>
   );
 };
 export default HomePage;
